ee385e79a50d26a29430281e837a3c21e959ef6b
Amar Jondhalekar <amarjondhalekar221297@gmail.com>
2024-11-18 11:31:03 +0530

    feat(middleware): creation of a middleware

--- index.js
+++ index.js
@@ -8,20 +8,24 @@ app.use(express.json());
 
 // middleware : logging, authentication, validation 
 
+// creation of a middleware 
 const loggingMiddleware = function(req, res, next){
     console.log('Logging');
     next();
 }
+app.use(loggingMiddleware);
 
 const authMiddleware = function(req, res, next){
     console.log('Authentication');
     next();
 }
+app.use(authMiddleware);
 
 const validationMiddleware = function(req, res, next){
     console.log('Validation');
     next();
 }
+app.use(validationMiddleware);
 
 app.get('/', (req, res) => {
     console.log(req.body);
